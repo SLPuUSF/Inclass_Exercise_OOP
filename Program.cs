80955196959797734cb1c9d7917081445c0d916a
Shaolin P <shiulan.poe@outlook.com>
2020-02-07 11:55:08 -0500

    Update

--- Program.cs
+++ Program.cs
@@ -3,33 +3,44 @@
 namespace Inclass_Exercise_OOP
 {
 
+    //class Car
+    //{
+    //    string color;
+    //    int maxSpeed;
+
+    //    static void Main(string[] args)
+    //    {
+    //        Car myObj = new Car();
+    //        myObj.color = "red";
+    //        myObj.maxSpeed = 200;
+    //        Console.WriteLine(myObj.color);
+    //        Console.WriteLine(myObj.maxSpeed);
+    //    }
+    //}
+
+    // Create a Car class
     class Car
     {
-        string color;
-        int maxSpeed;
+        public string model;  // Create a field
+
+        // Create a class constructor for the Car class
+        public Car()
+        {
+            model = "Mustang"; // Set the initial value for model
+        }
 
         static void Main(string[] args)
         {
-            Car myObj = new Car();
-            myObj.color = "red";
-            myObj.maxSpeed = 200;
-            Console.WriteLine(myObj.color);
-            Console.WriteLine(myObj.maxSpeed);
+            Car Ford = new Car();  // Create an object of the Car Class (this will call the constructor)
+            Console.WriteLine(Ford.model);  // Print the value of model
         }
     }
 
-    
+    // Outputs "Mustang"
 
-    // The class
-    class MyClass
-    {
-        // Class members
-        string color = "red";        // field
-        int maxSpeed = 200;          // field
-        public void fullThrottle()   // method
-        {
-            Console.WriteLine("The car is going as fast as it can!");
-        }
-    }
+
+
+
+
 
 }
