5ee4d8df81de4be031a0c526e6a69eb78b7e9149
Shaolin P <shiulan.poe@outlook.com>
2020-02-07 11:49:13 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Inclass_Exercise_OOP
+{
+
+    class Car
+    {
+        string color;
+        int maxSpeed;
+
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            myObj.color = "red";
+            myObj.maxSpeed = 200;
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.maxSpeed);
+        }
+    }
+
+    
+
+    // The class
+    class MyClass
+    {
+        // Class members
+        string color = "red";        // field
+        int maxSpeed = 200;          // field
+        public void fullThrottle()   // method
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+    }
+
+}
